fc032a328c907877e2aacf1de05b4f4c0c9f9791
Logan <logannormanthomas@protonmail.com>
2021-06-16 19:21:24 -0700

    fixed error caught by circle ci

--- src/components/__tests__/Application.test.js
+++ src/components/__tests__/Application.test.js
@@ -76,7 +76,7 @@ describe('Application', () => {
       queryByText(day, "Monday")
     );
   
-    expect(getByText(day, "1 spot remaining")).toBeInTheDocument();
+    expect(getByText(day, "2 spots remaining")).toBeInTheDocument();
   });
   
 
@@ -106,7 +106,7 @@ describe('Application', () => {
       );
       // console.log(prettyDOM(day));
   
-    expect(getByText(day, "2 spots remaining")).toBeInTheDocument();
+    expect(getByText(day, "1 spot remaining")).toBeInTheDocument();
 
 
   })
