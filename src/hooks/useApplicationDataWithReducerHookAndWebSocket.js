1eb84c7d5df2604ce584b94c3c51f613b46fbc53
Logan <logannormanthomas@protonmail.com>
2021-06-16 20:03:51 -0700

    got rid of dead comments

--- src/hooks/useApplicationDataWithReducerHookAndWebSocket.js
+++ src/hooks/useApplicationDataWithReducerHookAndWebSocket.js
@@ -3,44 +3,6 @@ import axios from "axios";
 import { updateSpots } from "helpers/updateSpots";
 import { reducer, SET_APPLICATION_DATA, SET_DAY, SET_INTERVIEW } from '../reducers/reducer';
 
-// const SET_DAY = "SET_DAY";
-// const SET_APPLICATION_DATA = "SET_APPLICATION_DATA";
-// const SET_INTERVIEW = "SET_INTERVIEW";
-
-
-// //reducer function for useReducer hook
-
-// const reducer = (state, action) => {
-//   console.log("action:", action);
-//   switch (action.type) {
-//     case SET_DAY:
-//       return { ...state, day: action.day };
-//     case SET_APPLICATION_DATA:
-//       return {
-//         ...state,
-//         days: action.days,
-//         appointments: action.appointments,
-//         interviewers: action.interviewers,
-//       };
-//     case SET_INTERVIEW: {
-//       console.log('state1234:', state, 'appointment:', action.appointment);
-
-//       //function imported to create a new days array with updated spots numbers to be rendered in the days list
-//       const days = updateSpots(state.day, state.days, {...state.appointments, [action.appointment.id]: action.appointment});
-
-//       return {
-//         ...state, 
-//         appointments: {...state.appointments, [action.appointment.id]: {...action.appointment, time: action.time}}, 
-//         days 
-//       }
-//     }
-//     default:
-//       throw new Error(
-//         `Tried to reduce with unsupported action type: ${action.type}`
-//       );
-//   }
-// };
-
 //initial state for reducer (cannot miss any states otherwise crash)
 const initialState = {
   day: "Monday",
@@ -86,14 +48,6 @@ export const useApplicationDataWithReducerHook = (initial) => {
           const appointment = JSON.parse(event.data);
 
           if (appointment.type === "SET_INTERVIEW") {
-            
-            const appointments = {
-              ...response.appointments,
-              [appointment.id]: {
-                time: response.appointments[appointment.id].time,
-                ...appointment
-              },
-            };
 
             dispatch({
               type: SET_INTERVIEW,
